525827e890ec730d4808092d7e2f726124cf7dfd
lb <lib@corp.wingoht.com>
2020-06-03 11:30:47 +0800

    first commit

---
+++ lib/Page/list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatefulWidget {
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      
+    );
+  }
+
+}
